defa239ebd3a97b8a7814d0c6bed5aedc65b8c83
yoyurec <yoyurec@gmail.com>
2023-10-04 02:30:36 +0300

    fix(left sidebar): compact menu and vaults

--- src/modules/extPlugins/calendar/calendar.ts
+++ src/modules/extPlugins/calendar/calendar.ts
@@ -76,7 +76,10 @@ const addCalendarButtonToSidebar = () => {
     const calendarButtonHTML = `
         <div class="calendar-nav">
             <a href="#${calendarPagePath}" class="item group flex items-center text-sm font-medium rounded-md" id="--awUi-calendar-menu">
-                <span class="ui__icon ti ls-icon-calendar-time"></span><span class="flex-1">Calendar</span>
+                <span class="ui__icon ti ls-icon-calendar-time">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M11 21H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3.5M16 3v4M8 3v4M4 11h11"></path><path d="M17.8 20.8 15.6 22a.4.4 0 0 1-.5-.5l.4-2.4-1.8-1.7a.4.4 0 0 1 .2-.6l2.5-.4 1-2.2a.4.4 0 0 1 .8 0l1 2.2 2.5.4a.4.4 0 0 1 .2.6L20 19.1l.4 2.4a.4.4 0 0 1-.5.5l-2.2-1.2z"></path></svg>
+                </span>
+                <span class="flex-1">Calendar</span>
                 </a>
         </div>
     `;
